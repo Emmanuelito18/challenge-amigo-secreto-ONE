094ac275c29ba8a47d392eb3ec73bbee521062f1
Emmanuelito18 <emmanuelito18yt@gmail.com>
2025-02-09 01:15:00 -0600

    Agregar función para agregar amigos y validar entradas en la aplicación "Amigo Secreto"

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo() {
+    let nombreAmigo;//Se utiliza para almacenar el nombre del amigo
+    nombreAmigo = document.getElementById('amigo').value;//obtiene el valor del input
+
+    if (nombreAmigo === "") {//Comprueba si el nombre no está vacío
+        alert("Por favor ingrese un nombre válido");
+    } else {
+        amigos.push(nombreAmigo);//Agrega el nombre al array
+    }
+}
